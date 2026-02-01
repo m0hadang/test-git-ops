d14f266c9d359bf9b836ce385fd46d25af502819
mohadang <ehdlaos@gmail.com>
2026-02-01 12:43:19 +0900

    [mod] add webserver function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
